e6389c533aa7caec7c20c7196b24335af1afe803
David Gibb <degibb@gmail.com>
2017-05-13 17:13:35 -0700

    screwing around with docker image build path

--- ci/task_build_rc.sh
+++ ci/task_build_rc.sh
@@ -5,6 +5,8 @@ set -e
 VERSION=$(cat version/number)
 ARTIFACT=$(pwd)/build-artifacts/simple-go-web-app-$VERSION
 
+cp pipeline/Dockerfile build-artifacts/
+
 # application input is in gopath/src/github.com/cloudfoundry-community/simple-go-web-app folder
 # $GOPATH is gopath/ folder
 export GOPATH=$(pwd)/gopath:$(pwd)/gopath/src/github.com/cloudfoundry-community/simple-go-web-app/Godeps/_workspace
